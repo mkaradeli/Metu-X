20612c3b02740325ac8b436460d28832c9e9edce
mkaradeli <59120408+mkaradeli@users.noreply.github.com>
2026-09-16 22:37:37 +0300

    acceleration feedforward hatasi cozulmus, gainler guncellendi.

--- utilities/platformControllerTargets.m
+++ utilities/platformControllerTargets.m
@@ -5,12 +5,12 @@ platform_targets.h_cut   = 0.1; % m 0.3
 platform_targets.h_blend = 1; % m
 
 
-platform_targets.a_dec =3; %11.3
+platform_targets.a_dec =3.5; %11.3
 % platform_targets.a_dec_high = 15; % m/s2
 platform_targets.a_dec_low = 5; % m/s2
 
 
-platform_targets.V_max = 4; % m/s
+platform_targets.V_max = 4.7; % m/s
 platform_targets.Ts = 1/50;
 platform_targets.fs = 50;
 % platform_targets.v_blend = sqrt( platform_targets.V_td^2 + 2*platform_targets.a_dec_low*(platform_targets.h_blend - platform_targets.h_cut));  %  /* precompute, constant */
